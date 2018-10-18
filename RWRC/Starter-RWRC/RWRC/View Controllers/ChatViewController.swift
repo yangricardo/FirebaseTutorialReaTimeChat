6eb2f88ff5abc2f57531a493aae326886e6a14c0
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-10-18 12:45:10 -0300

    handling images send

--- RWRC/Starter-RWRC/RWRC/View Controllers/ChatViewController.swift
+++ RWRC/Starter-RWRC/RWRC/View Controllers/ChatViewController.swift
@@ -30,6 +30,7 @@ import UIKit
 import Firebase
 import MessageKit
 import FirebaseFirestore
+import Photos
 
 final class ChatViewController: MessagesViewController {
   
@@ -42,6 +43,19 @@ final class ChatViewController: MessagesViewController {
   
   private let db = Firestore.firestore()
   private var reference: CollectionReference?
+
+  
+  private var isSendingPhoto = false {
+    didSet {
+      DispatchQueue.main.async {
+        self.messageInputBar.leftStackViewItems.forEach { item in
+          item.isEnabled = !self.isSendingPhoto
+        }
+      }
+    }
+  }
+  
+  private let storage = Storage.storage().reference()
 
 
   deinit {
@@ -97,10 +111,91 @@ final class ChatViewController: MessagesViewController {
     messagesCollectionView.messagesLayoutDelegate = self
     messagesCollectionView.messagesDisplayDelegate = self
 
+    
+    
+    
+    // 1 Create a new InputBarButtonItem with a tint color and an image.
+    let cameraItem = InputBarButtonItem(type: .system)
+    cameraItem.tintColor = .primary
+    cameraItem.image = #imageLiteral(resourceName: "camera")
+    
+    // 2 Connect the new button to cameraButtonPressed().
+    cameraItem.addTarget(
+      self,
+      action: #selector(cameraButtonPressed),
+      for: .primaryActionTriggered
+    )
+    cameraItem.setSize(CGSize(width: 60, height: 30), animated: false)
+    
+    messageInputBar.leftStackView.alignment = .center
+    messageInputBar.setLeftStackViewWidthConstant(to: 50, animated: false)
+    
+    // 3 Lastly, add the item to the left side of the message bar.
+    messageInputBar.setStackViewItems([cameraItem], forStack: .left, animated: false)
+  }
+
+  
+  
+  // MARK: - Actions
+  
+  @objc private func cameraButtonPressed() {
+    // This method will present an image picker controller to allow the user to select an image.
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      picker.sourceType = .camera
+    } else {
+      picker.sourceType = .photoLibrary
+    }
+    
+    present(picker, animated: true, completion: nil)
   }
 
   
   // MARK: - Helpers
+  private func uploadImage(_ image: UIImage, to channel: Channel, completion: @escaping (URL?) -> Void) {
+    guard let channelID = channel.id else {
+      completion(nil)
+      return
+    }
+    
+    guard let scaledImage = image.scaledToSafeUploadSize,
+      let data = scaledImage.jpegData(compressionQuality: 0.4) else {
+        completion(nil)
+        return
+    }
+    
+    let metadata = StorageMetadata()
+    metadata.contentType = "image/jpeg"
+    
+    let imageName = [UUID().uuidString, String(Date().timeIntervalSince1970)].joined()
+    storage.child(channelID).child(imageName).putData(data, metadata: metadata) { meta, error in
+      completion(meta?.downloadURL())
+    }
+  }
+  
+  private func sendPhoto(_ image: UIImage) {
+    isSendingPhoto = true
+    
+    uploadImage(image, to: channel) { [weak self] url in
+      guard let `self` = self else {
+        return
+      }
+      self.isSendingPhoto = false
+      
+      guard let url = url else {
+        return
+      }
+      
+      var message = Message(user: self.user, image: image)
+      message.downloadURL = url
+      
+      self.save(message)
+      self.messagesCollectionView.scrollToBottom()
+    }
+  }
+  
   private func save(_ message: Message) {
     /*This method uses the reference that was just setup. The addDocument method on the reference takes a dictionary with the keys and values that represent that data. The message data struct implements DatabaseRepresentation, which defines a dictionary property to fill out.*/
     reference?.addDocument(data: message.representation) { error in
@@ -135,16 +230,45 @@ final class ChatViewController: MessagesViewController {
   }
 
   private func handleDocumentChange(_ change: DocumentChange) {
-    guard let message = Message(document: change.document) else {
+    guard var message = Message(document: change.document) else {
       return
     }
     
     switch change.type {
     case .added:
-      insertNewMessage(message)
+      if let url = message.downloadURL {
+        downloadImage(at: url) { [weak self] image in
+          guard let self = self else {
+            return
+          }
+          guard let image = image else {
+            return
+          }
+          
+          message.image = image
+          self.insertNewMessage(message)
+        }
+      } else {
+        insertNewMessage(message)
+      }
+
       
     default:
       break
+    }
+  }
+  
+  private func downloadImage(at url: URL, completion: @escaping (UIImage?) -> Void) {
+    let ref = Storage.storage().reference(forURL: url.absoluteString)
+    let megaByte = Int64(1 * 1024 * 1024)
+    
+    ref.getData(maxSize: megaByte) { data, error in
+      guard let imageData = data else {
+        completion(nil)
+        return
+      }
+      
+      completion(UIImage(data: imageData))
     }
   }
 
@@ -236,6 +360,37 @@ extension ChatViewController: MessageInputBarDelegate {
 
 extension ChatViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
   
+  func imagePickerController(_ picker: UIImagePickerController,
+                             didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    picker.dismiss(animated: true, completion: nil)
+    
+    // 1 If the user selected an asset, the selected image needs to be downloaded from iCloud. Request it at a fixed size. Once it’s successfully retrieved, send it.
+    if let asset = info[.phAsset] as? PHAsset {
+      let size = CGSize(width: 500, height: 500)
+      PHImageManager.default().requestImage(
+        for: asset,
+        targetSize: size,
+        contentMode: .aspectFit,
+        options: nil) { result, info in
+          
+          guard let image = result else {
+            return
+          }
+          
+          self.sendPhoto(image)
+      }
+      
+      // 2 If there is an original image in the info dictionary, send that. You don’t need to worry about the original image being too large here because the storage helper handles resizing the image for you. Have a look at UIImage+Additions.swift to see how the resizing is done.
+
+    } else if let image = info[.originalImage] as? UIImage {
+      sendPhoto(image)
+    }
+  }
+  
+  func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    picker.dismiss(animated: true, completion: nil)
+  }
+
 }
 
 
